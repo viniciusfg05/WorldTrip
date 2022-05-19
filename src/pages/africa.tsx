5aad92c58d16db8d14bd66f401b72ee9bd9f9afb
unknown <viniciusserlimpa@gmail.com>
2022-05-19 12:56:03 -0300

    spinner

--- src/pages/africa.tsx
+++ src/pages/africa.tsx
@@ -38,7 +38,7 @@ export default function Africa() {
       <Header/>
       { isLoading ? (
         <Flex justifyContent="center" alignItems="center" h="100vh">
-          <Spinner w="200px" h="200px"/>
+          <Spinner w={{sm: "50px", md: "200px"}} h={{sm: "50px", md: "200px"}}/>
         </Flex>
       ) : error ? (
         <Flex justifyContent="center">
